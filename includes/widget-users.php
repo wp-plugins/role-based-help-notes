9d93b8528a5eb96566efc695e2868a8fecbad72f
JustinTicktock <JustinTicktock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-06 13:22:48 +0000

    = 1.2.8 = * 2014-03-06 * Re-factored code into Classes. * Settings screen now in tabs. * Database handling of options simplified, dropped upgrade support pre 1.2.7 version. * New feature: Added the suggested plugin extension ["user_switching"](http://wordpress.org/plugins/user-switching/). * New feature: New error message on attempting to disable a plugin that is being forced active by plugin extensions.
    git-svn-id: https://plugins.svn.wordpress.org/role-based-help-notes/tags/1.2.8@870588 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/widget-users.php
@@ -0,0 +1,137 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Adds Users_Widget widget.
+ */
+class Users_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'users_widget', // Base ID
+			__('Help Note Users', 'role-based-help-notes-text-domain'), // Name
+			array( 'description' => __( 'A Users Widget', 'role-based-help-notes-text-domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		global $role_based_help_notes;
+	
+       // drop out if not a single Help Note page or Help Hote Archive page.
+       // or the General Help Note Type
+       $show_widget_help_notes = $role_based_help_notes->rbhn_active_posttypes();
+       $exclude_help_notes = array('h_general');
+       $show_widget_help_notes = array_diff($show_widget_help_notes, $exclude_help_notes);
+       
+        if ( ! in_array( get_post_type(),  $show_widget_help_notes ) || is_archive()  )
+            return; 
+
+		$post_type = get_post_type();
+		$help_note_object = get_post_type_object( $post_type );
+		$help_note_name = $help_note_object->labels->menu_name;
+		
+		if ( empty( $instance['title'] ) ) {
+			$title = sprintf( __( '%1$s Line-up', 'role-based-help-notes-text-domain'), $help_note_name);
+		} else {
+			$title = $instance['title'];
+		}
+		
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+        
+		// Find the role based on the post type.
+		$post_type = get_post_type();
+		$post_types_array = get_option('rbhn_post_types');
+		$help_note_role =  '';
+		if (  ! empty($post_types_array ) ) {
+			foreach( $post_types_array as $active_role=>$active_posttype) {
+				if ($post_type == $active_posttype) {
+					$help_note_role =  $active_role;
+					break 2;
+				}
+			}
+		}
+
+        $users = get_users( Array('role' => $help_note_role) );
+
+		/* If users were found. */
+		if ( !empty( $users ) ) {
+            
+			echo '<ul class="xoxo users">';
+
+			/* Loop through each available user, creating a list item with a link to the user's archive. */
+			foreach ( $users as $user ) {
+
+				$user_id = $user->ID;
+				$url = apply_filters( 'rbhn_author_url', get_author_posts_url( $user_id, $user->user_nicename ) , $user_id);	
+
+				$class = "user-{$user->ID}";
+				if ( is_author( $user->ID ) )
+					$class .= ' current-user';
+
+				echo "<li class='{$class}'><a href='{$url}' title='" . esc_attr( $user->display_name ) . "'>{$user->display_name}</a></li>\n";
+			}
+
+			echo '</ul>';
+		}
+        
+        
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) && ( $instance[ 'title' ] != "" )) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = '';
+		}
+
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		<?php echo __('Leave Blank for a dynamic title which includes the Role.', 'role-based-help-notes-text-domain'); ?></p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Users_Widget
+
+?>
