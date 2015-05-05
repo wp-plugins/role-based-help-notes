329edbb581e845854c0158a0c85a83b4b3c252f3
JustinTicktock <JustinTicktock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 18:44:19 +0000

    cleaned email-users/email-users-bcc-2-to.php
    git-svn-id: https://plugins.svn.wordpress.org/role-based-help-notes/trunk@1154075 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin-compatibility/email-users/email-users-bcc-2-to.php
+++ includes/plugin-compatibility/email-users/email-users-bcc-2-to.php
@@ -17,29 +17,19 @@ function mailusers_rbhn_headers($to, $headers, $bcc)
 }
 
 $Role_Based_Help_Notes = RBHN_Role_Based_Help_Notes::get_instance( );
-//$user_ID = get_current_user_id();
-//$help_note_roles = $role_based_help_notes->active_help_notes( $user_ID );
 $help_note_roles = $role_based_help_notes->help_notes_role( );
         
 // add conditionals for the filter moving email addresses from  BCC > TO
 
 if ( isset($_POST['send_targets']) && is_array($_POST['send_targets']) && count($_POST['send_targets']) == 1 ) {    // limit to only where one group is selected.
-                                                                                                                    // if the email is "To" only one group (implies that it could be a help note related email.
-               
-    $send_2_email_users_group = $_POST['send_targets'];
-    $send_2_role = preg_replace('/role-/', '', $send_2_email_users_group) ;
+                                                                                                                    // if the email is "To" only one group (implies that it could be a help note related email.          
+    $selected_email_users_group = $_POST['send_targets'];
+    $send_2_role = preg_replace('/role-/', '', $selected_email_users_group) ;
     
-   // if ( array_key_exists( array_shift(array_values($send_2_role)), $help_note_roles ) ) {  // and if the group/role has help notes enabled
      if ( in_array( array_shift(array_values($send_2_role)), $help_note_roles ) ) {  // and if the group/role has help notes enabled
-        die();
-    }
-    
-    
-      add_filter('mailusers_manipulate_headers', 'mailusers_rbhn_headers', 10, 3) ;  
+        add_filter('mailusers_manipulate_headers', 'mailusers_rbhn_headers', 10, 3) ;  
+    }     
 }
-
-                
-
 
 
 ?>
